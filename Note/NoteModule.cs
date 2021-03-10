33d725b905d16cf9a2e6a9489311954e92215eef
unknown <sauageog@gmail.com>
2021-03-10 18:33:52 +0200

    Simple setting M and VM

---
+++ Note/NoteModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Note
+{
+    class NoteModule
+    {
+        private List<string> notes = new List<string>();
+        public ReadOnlyCollection<string> Notes;
+
+        public NoteModule()
+        {
+            Notes = new ReadOnlyCollection<string>(notes);
+        }
+
+        public void AddToArray(string value)
+        {
+            notes.Add(value);
+        }
+    }
+}
---
+++ Note/NoteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using System.Collections.ObjectModel;
+
+namespace Note
+{
+    class NoteViewModel
+    {
+        NoteModule noteM;
+
+        public ReadOnlyCollection<string> NoteView => noteM.Notes;
+
+        DelegateCommand
+
+        public NoteViewModel()
+        {
+            noteM = new NoteModule();
+        }
+    }
+}
